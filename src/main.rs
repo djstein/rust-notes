8bd28e6a2138c4132b7288caeb74c0506ac331b1
Dylan Stein <dylan@steins.studio>
2023-11-29 21:29:56 -0800

    add remove from vec

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,28 @@ fn main() {
     for p in (0..=v3.len() - 1).rev() {
         println!("{}", v3[p]);
     }
+
+    // REMOVE an element from a vector
+    let mut v4 = vec![1, 2, 3, 4, 5];
+    let needle = 3;
+    // Option 1: Remove first instance equal to needle O(n)
+    v4.remove(
+        v4.iter()
+            .position(|x| *x == needle)
+            .expect("needle not found"),
+    );
+    // Option 2: Remove last instance equal to needle O(n)
+    v4.remove(
+        v4.iter()
+            .rposition(|x| *x == needle)
+            .expect("needle not found"),
+    );
+    // Option 2: Remove all instances equal to needle
+    v4.retain(|x| *x != needle);
+    // Option 3: Remove and swap by shifting indices to last element O(1)
+    v4.swap_remove(
+        v4.iter()
+            .position(|x| *x == needle)
+            .expect("needle not found"),
+    );
 }
